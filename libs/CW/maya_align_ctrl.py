88bbe826192a3af5b71d814503653402f3f72428
rotoslinger <harrison.levi.l@gmail.com>
2024-12-10 11:47:59 -0800

    Got the transfer working very well! About to start working on automating it

--- libs/CW/maya_align_ctrl.py
+++ libs/CW/maya_align_ctrl.py
@@ -19,19 +19,36 @@ def create_space_switch(ctrl,
                         make_names: list | None = None, # list of names to create xforms for
                         make_parents: list | None = None, # where to parent the new xforms 
                         space_dict: dict = {'World':'world_null',
-                                            'Chest':'Spine2'}):
+                                            'Chest':'Spine2'},
+                        follow=False
+                        ):
     '''
     creates new transforms for spaces and a space switch attribute for the specified control
     '''
+
+    if follow:
+        # NOTE: where you store the rot/pos of the control before the switch happens.
+        add_storage_attrs(ctrl, attr_name='cur_rotate')
+        add_storage_attrs(ctrl, attr_name='cur_translate')
+        if not cmds.objExists(f'{ctrl}.FOLLOW'):
+            cmds.addAttr(ctrl, longName='FOLLOW', attributeType='bool',
+                            keyable=False, hidden=True)
+
     enum_list = space_dict.keys()
     enum_str = ':'.join(enum_list)
 
     if not cmds.objExists(f'{ctrl}.align'):
         cmds.addAttr(ctrl, longName='align', attributeType='enum',
                      enumName=enum_str, keyable=True)
+        
+    # NOTE: may not be needed...    
     if not cmds.objExists(f'{ctrl}.align_cur_state'):
         cmds.addAttr(ctrl, longName='align_cur_state', attributeType='enum',
                      enumName=enum_str, keyable=False, hidden=True)
+    
+    # NOTE: where you store the position of the control before the switch happens.
+    if not cmds.objExists(f'{ctrl}.ctrl_cur_matrix'):
+        cmds.addAttr(ctrl, ln='ctrl_cur_matrix', at='matrix', keyable=False, hidden=True)
 
     if make_names and make_parents:
         for name, parent in zip(make_names, make_parents):
@@ -42,3 +59,35 @@ def create_space_switch(ctrl,
                           lock=True) for attr in ('translate', 'rotate', 'scale') for axis in 'XYZ']
             if 'world' in name:
                 cmds.setAttr(f'{new_node}.inheritsTransform', 0)
+
+    constraints = cmds.listConnections(ctrl, type="constraint", source=True, destination=False)
+
+
+def add_storage_attrs(ctrl, attr_name='cur_rotate'):
+    """
+    Adds a compound attribute to store attributes used for compensation
+    cur_rotate_x, cur_rotate_y, cur_rotate_z or cur_translate_x, cur_translate_y, cur_translate_z
+    """
+    if cmds.objExists(f'{ctrl}.{attr_name}'):
+        if not cmds.about(batch=True):
+            print(f'Attribute "{attr_name}" already exists on "{ctrl}".')
+        return
+    
+    # Add the compound attribute for rotations
+    cmds.addAttr(ctrl, ln=attr_name, at='compound', numberOfChildren=3, keyable=False, hidden=True)
+    cmds.addAttr(ctrl, ln=f'{attr_name}_x', at='double', parent=attr_name,
+                 keyable=False, hidden=True)
+    cmds.addAttr(ctrl, ln=f'{attr_name}_y', at='double', parent=attr_name,
+                 keyable=False, hidden=True)
+    cmds.addAttr(ctrl, ln=f'{attr_name}_z', at='double', parent=attr_name,
+                 keyable=False, hidden=True)
+
+    return f'{ctrl}.{attr_name}', f'{attr_name}_x', f'{attr_name}_y', f'{attr_name}_z'
+    
+def buffer_in_place(maya_obj, new_buffer_name):
+    """Parent maya object to new transform at the same world position and hier location."""
+    parent = cmds.listRelatives(maya_obj, parent=True)
+    buffer = cmds.createNode('transform', parent=parent, name=new_buffer_name)
+    cmds.matchTransform(buffer, maya_obj)
+    cmds.parent(maya_obj, buffer)
+    return buffer
---
+++ libs/CW/maya_align_script_node.py
@@ -0,0 +1,10 @@
+from maya import cmds
+
+# get the world space matrix of the control before the align switch
+# set it in a hidden attribute
+
+# do the switch
+
+# retrieve the hidden attribute
+# set the hidden attribute on the control.
+
